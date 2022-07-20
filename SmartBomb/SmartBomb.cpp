d8d447db7b97119ecc37ce707d4717dd00b78570
gor111111 <gor.stepanyan.2020@inbox.ru>
2022-07-20 15:41:32 +0400

    changes

--- SmartBomb/SmartBomb.cpp
+++ SmartBomb/SmartBomb.cpp
@@ -2,9 +2,42 @@
 
 using namespace std;
 
+//enum class Keys {
+//
+//	    left = 75,
+//		right = 77,
+//		top = 72,
+//		down = 80,
+//
+//};
 int main()
 {
-	cout << "Hello CMake." << endl;
+	//72 verev slaq		
+	//80 nerqev slaq
+	//75 dzax slaq
+	//77 ach slaq
+	
+    /*int k;
+    cout << "hraman-> ";
+    cin >> k;
+       
+        switch (k)
+        {
+        case 72: cout << "Up\n";
+            break;
+        case 77: cout << "RIGHT\n";
+            break;
+        case 75: cout << "LEFT\n";
+            break;
+        case 80: cout << "DOWN\n";
+            break;
 
-	return 0;
-}
+        }
+        return 0;*/
+
+
+   
+
+
+    }
+
