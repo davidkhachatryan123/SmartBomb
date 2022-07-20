72f077bcf81c9e3b8691471c692a4dd226354bf4
davidkhachatryan123 <xdavit777@icloud.com>
2022-07-20 10:21:32 +0400

    20-Jul-22 10:21

--- SmartBomb/SmartBomb.cpp
+++ SmartBomb/SmartBomb.cpp
@@ -1,12 +1,10 @@
-﻿// SmartBomb.cpp : Defines the entry point for the application.
-//
-
-#include "SmartBomb.h"
+﻿#include "SmartBomb.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+
 	return 0;
 }
