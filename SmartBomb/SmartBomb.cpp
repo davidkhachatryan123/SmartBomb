cda11d074f63eb361e182aca484782b26820bd59
ArsenDarbinyan <75303589+ArsenDarbinyan@users.noreply.github.com>
2022-07-21 09:26:10 +0400

    fixing

--- SmartBomb/SmartBomb.cpp
+++ SmartBomb/SmartBomb.cpp
@@ -4,16 +4,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-	Mat img = imread("/home/david/Pictures/tree.jpg");
-
-	imshow("Tree", img);
-	imshow("Tree2", img);
-
-	waitKey(0);
-=======
 	cout << "Programm Skeleton." << endl;
->>>>>>> 'main'
 
 	return 0;
 }
