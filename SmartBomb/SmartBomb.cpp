3e6a451cc786078f964e007f517381b51aeb597f
davidkhachatryan123 <xdavit777@icloud.com>
2022-07-22 11:05:44 +0400

    changed parametrers for function MoveX();

--- SmartBomb/SmartBomb.cpp
+++ SmartBomb/SmartBomb.cpp
@@ -5,7 +5,25 @@ using namespace cv;
 
 int main()
 {
-	
+	VideoCapture cap;
+
+	if (CaptureLocalVideo::OpenCapture("/home/david/Videos/video.mp4", cap))
+	{
+		Mat frame;
+
+		while (CaptureLocalVideo::Get(cap, frame))
+		{
+			Draw::drawX(Coordinates(0.0f, +0.5f), frame);
+
+			LocalDisplay::Send("Window", frame);
+
+			waitKey(33);
+		}
+	}
+
+
+	waitKey(0);
+	cap.release();
 
 	return 0;
 }
